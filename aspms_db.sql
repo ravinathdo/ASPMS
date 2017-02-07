c2c5f75c4d932098982b6e23479419ac76399063
kamani <kamani@kamani-PC>
2017-02-07 09:14:54 -0800

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

---
+++ aspms_db.sql
@@ -0,0 +1,59 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.1.37-community : Database - aspms_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`aspms_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `aspms_db`;
+
+/*Table structure for table `item` */
+
+DROP TABLE IF EXISTS `item`;
+
+CREATE TABLE `item` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(50) DEFAULT NULL,
+  `category_code` int(10) DEFAULT NULL,
+  `brand` varchar(100) DEFAULT NULL,
+  `buying_price` varchar(10) DEFAULT NULL,
+  `selling_price` varchar(10) DEFAULT NULL,
+  `qty` int(5) DEFAULT NULL,
+  `image_path` varchar(200) DEFAULT NULL,
+  `store_area` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `item` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` text,
+  `firstname` varchar(200) DEFAULT NULL,
+  `lastname` varchar(200) DEFAULT NULL,
+  `role` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`firstname`,`lastname`,`role`) values (1,'admin','*667F407DE7C6AD07358FA38DAED7828A72014B4E','admin','admin','ADM'),(2,'saman','*667F407DE7C6AD07358FA38DAED7828A72014B4E','saman','kumara','EMP');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
