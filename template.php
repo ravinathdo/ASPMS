cae8e3de6cd7746600144b82f0508ec3111dd030
001343 <001343@ITDEV17.unionb.com>
2017-02-07 16:47:48 +0530

    initial project

--- template.php
+++ template.php
@@ -4,12 +4,12 @@ Author: W3layouts
 Author URL: http://w3layouts.com
 License: Creative Commons Attribution 3.0 Unported
 License URL: http://creativecommons.org/licenses/by/3.0/
-Ravinath Fernando
+Ravinath Fernando Gayathri
 -->
 <!DOCTYPE HTML>
 <html>
     <head>
-        <title>ASPMS</title>
+        <title>ASPMS-Template</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="keywords" content="" />
@@ -59,7 +59,7 @@ Ravinath Fernando
                         <div class="row">
                             <div class="col-md-12"> <i class="fa fa-car fa-3"></i>
                                 <b> Automobile Spare Part Management System </b>
-                                 <p>Automotive ERP system</p>
+                                <p> Automotive ERP system</p>
                             <hr>
                             </div>
                         </div>
