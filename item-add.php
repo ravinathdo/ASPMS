57e5acc7e7c97b93bc5ab9897527414f3266b8f3
001343 <001343@ITDEV17.unionb.com>
2017-02-08 09:48:37 +0530

    initial project

--- item-add.php
+++ item-add.php
@@ -5,6 +5,10 @@ Author URL: http://w3layouts.com
 License: Creative Commons Attribution 3.0 Unported
 License URL: http://creativecommons.org/licenses/by/3.0/
 -->
+<?php
+session_start();
+include './_functions.php';
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
@@ -70,52 +74,98 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                                 <div class="chrt">
                                     <b> Item Add</b>
 
-                                    <form name="form1" method="post" action="">
+                                    <form name="form1" method="post" action="item-add.php" enctype="multipart/form-data">
                                         <table width="100%" border="0" cellspacing="5" cellpadding="0">
                                             <tr>
                                                 <td width="15%">Item Name</td>
-                                                <td width="47%"><input type="text" name="textfield" id="textfield"></td>
+                                                <td width="47%"><input type="text" name="item_name" id="item_name"></td>
                                                 <td width="28%">Store Area</td>
-                                                <td width="10%"><select name="select2" id="select2">
-                                                        <option>--select--</option>
+                                                <td width="10%"><select name="store_area" id="store_area">
+                                                        <option value="">--select--</option>
                                                         <option>E1</option>
                                                         <option>E2</option>
                                                     </select></td>
                                             </tr>
                                             <tr>
                                                 <td>Category</td>
-                                                <td><select name="select" id="select">
-                                                        <option>--select--</option>
+                                                <td><select name="category_code" id="category_code">
+                                                        <option value="">--select--</option>
+                                                        <option value="Engine">Engine</option>
                                                     </select></td>
                                                 <td>Image</td>
-                                                <td><input type="file"/></td>
+                                                <td><input type="file" name="image"/></td>
                                             </tr>
                                             <tr>
                                                 <td>Brand</td>
-                                                <td><input type="text" name="textfield2" id="textfield2"></td>
+                                                <td><input type="text" name="brand" id="brand"></td>
                                                 <td>Buying Price</td>
-                                                <td><input type="text" name="textfield3" id="textfield3"></td>
+                                                <td><input type="text" name="buying_price" id="buying_price"></td>
                                             </tr>
                                             <tr>
                                                 <td>Description</td>
-                                                <td><textarea name="textarea" id="textarea"></textarea></td>
+                                                <td><textarea name="description" id="description"></textarea></td>
                                                 <td>Selling Price</td>
-                                                <td><input type="text" name="textfield4" id="textfield4"></td>
+                                                <td><input type="text" name="selling_price" id="selling_price"></td>
                                             </tr>
                                             <tr>
                                                 <td>&nbsp;</td>
                                                 <td>&nbsp;</td>
                                                 <td>Qty</td>
-                                                <td><input type="text" name="textfield5" id="textfield5"></td>
+                                                <td><input type="text" name="qty" id="qty"></td>
                                             </tr>
                                             <tr>
-                                                <td><input type="submit" value="Add Item"/></td>
+                                                <td><input type="submit" value="Add Item" name="btnadditem"/></td>
                                                 <td>&nbsp;</td>
                                                 <td>&nbsp;</td>
                                                 <td>&nbsp;</td>
                                             </tr>
                                         </table>
                                     </form>
+
+                                    <?php
+//                                    if (isset($_FILES['image'])) {
+//                                        $errors = array();
+//                                        $file_name = $_FILES['image']['name'];
+//                                        $file_size = $_FILES['image']['size'];
+//                                        $file_tmp = $_FILES['image']['tmp_name'];
+//                                        $file_type = $_FILES['image']['type'];
+//                                        $file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));
+//
+//                                        $expensions = array("jpeg", "jpg", "png");
+//
+//                                        if (in_array($file_ext, $expensions) === false) {
+//                                            $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+//                                        }
+//
+//                                        if ($file_size > 2097152) {
+//                                            $errors[] = 'File size must be excately 2 MB';
+//                                        }
+//
+//                                        if (empty($errors) == true) {
+//                                            move_uploaded_file($file_tmp, "item_images/" . $file_name);
+//                                            
+//                                            
+//                                            
+//                                            //add data into item table
+//                                            
+//                                            echo 'Item name: ';$_FILES['item_name'];
+//                                            
+//                                            
+//                                            echo "Success";
+//                                        } else {
+//                                            print_r($errors);
+//                                        }
+//                                    }
+
+
+
+                                    if (isset($_POST['btnadditem'])) {
+                                        
+                                        addItem();
+                                    }
+                                    ?>
+
+
                                 </div>
                             </div>
                             <div class="col-lg-2">
@@ -127,37 +177,51 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 
                                 <table width="100%" border="0" cellspacing="0" cellpadding="0" class="table-bordered">
                                     <tr class="success">
-                                      <td width="7%"><strong>Item No</strong></td>
+                                        <td width="7%"><strong>Item No</strong></td>
                                         <td width="13%"><strong>Item Name</strong></td>
                                         <td width="8%"><strong>Category</strong></td>
                                         <td width="10%"><strong>Stock Area</strong></td>
                                         <td width="9%"><strong>Stock Qty</strong></td>
-                                        <td width="43%"><strong>Category</strong></td>
+                                        <td width="43%"><strong>Description</strong></td>
                                         <td width="10%">&nbsp;</td>
                                         <td width="10%">&nbsp;</td>
                                     </tr>
+
+                                    <?php
+//show item list
+                                    $con_s = getDBConnection();
+                                    $sql = "SELECT * FROM item";
+                                    $result = mysqli_query($con_s, $sql);
+
+                                    if (mysqli_num_rows($result) > 0) {
+                                        // output data of each row
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                           // echo "id: " . $row["item_name"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
+                                            ?>
+                                            <tr>
+                                                <td><?php echo $row["id"]?></td>
+                                                <td><?php echo $row["item_name"]?></td>
+                                                <td><?php echo $row["category_code"]?></td>
+                                                <td><span class="btn-primary"> <?php echo $row["store_area"]?> </span>&nbsp;</td>
+                                                <td><?php echo $row["qty"]?></td>
+                                                <td><?php echo $row["description"]?></td>
+                                                <td><a href="item-update.php">Update</a></td>
+                                                <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                                            </tr>
+                                            <?php
+                                        }
+                                    } else {
+                                        echo "0 results";
+                                    }
+
+                                    mysqli_close($con_s);
+                                    ?>
+
+
+
+                                   
                                     <tr>
-                                      <td>1</td>
-                                        <td>Engin-Toyota</td>
-                                        <td>Engin</td>
-                                        <td><span class="btn-primary"> E1 </span>&nbsp;</td>
-                                        <td>10</td>
-                                        <td>On  the Insert tab, the galleries include items that are designed </td>
-                                        <td><a href="item-update.php">Update</a></td>
-                                        <td><i class="fa fa-times" aria-hidden="true"></i></td>
-                                    </tr>
-                                    <tr>
-                                      <td>2</td>
-                                        <td>Radiator-Toyota</td>
-                                        <td>Radiator</td>
-                                        <td><span class="btn-primary">E1 </span>&nbsp;</td>
-                                        <td>20</td>
-                                        <td>On  the Insert tab, the galleries include items that are designed </td>
-                                        <td><a href="#">Update</a></td>
-                                        <td><i class="fa fa-times" aria-hidden="true"></i></td>
-                                    </tr>
-                                    <tr>
-                                      <td>&nbsp;</td>
+                                        <td>&nbsp;</td>
                                         <td>&nbsp;</td>
                                         <td>&nbsp;</td>
                                         <td>&nbsp;</td>
@@ -204,7 +268,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                 </div>
                 <!--//down-->
                 <div class="menu">
-                   <?php include('menu-customer.php')?>
+                    <?php include('menu-customer.php') ?>
                 </div>
             </div>
             <div class="clearfix"></div>		
