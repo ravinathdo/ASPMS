b375bdc23c6179e28d19b618ee1e87e98a173f8f
001343 <001343@ITDEV17.unionb.com>
2017-02-08 09:48:49 +0530

    initial project

--- _functions.php
+++ _functions.php
@@ -24,16 +24,70 @@ function getDBConnection() {
 
 function doLogin() {
     $conn = getDBConnection();
-    $sql = "SELECT * FROM USERS WHERE username = '".$_POST['username']."' AND password = PASSWORD('".$_POST['password']."')";
-    echo 'sql:'.$sql;
-    $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
-        }
+
+
+    //login hard-corded
+    if ($_POST['username'] == 'admin' && $_POST['password'] == 'a') {
+        $_SESSION['islog'] = true;
+        $_SESSION['username'] = "admin";
+        $_SESSION['role'] = "ADM";
+        return true;
+    } else if ($_POST['username'] == 'emp' && $_POST['password'] == 'a') {
+        $_SESSION['islog'] = true;
+        $_SESSION['username'] = "emp";
+        $_SESSION['role'] = "EMP";
+        return true;
+    }
+    return false;
+
+
+
+    /*
+      $sql = "SELECT * FROM USERS WHERE username = '".$_POST['username']."' AND password = PASSWORD('".$_POST['password']."')";
+      echo 'sql:'.$sql;
+      $result = mysqli_query($conn, $sql);
+      if (mysqli_num_rows($result) > 0) {
+      // output data of each row
+      while ($row = mysqli_fetch_assoc($result)) {
+      echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
+      }
+      } else {
+      echo "0 results";
+      }
+
+      mysqli_close($conn);
+
+     */
+}
+
+function addItem() {
+    //insert into table
+     $conn = getDBConnection();
+    
+    $sql = " INSERT INTO item
+            (item_name,
+             category_code,
+             brand,
+             buying_price,
+             selling_price,
+             description,
+             qty,
+             image_path,
+             store_area)
+VALUES ('".$_POST['item_name']."',
+        '".$_POST['category_code']."',
+        '".$_POST['brand']."',
+        '".$_POST['buying_price']."',
+        '".$_POST['selling_price']."',
+        '".$_POST['description']."',
+        '".$_POST['qty']."',
+        'sample.jpg',
+        '".$_POST['store_area']."'); ";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
     } else {
-        echo "0 results";
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 
     mysqli_close($conn);
